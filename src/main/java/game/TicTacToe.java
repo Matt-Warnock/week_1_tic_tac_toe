cf84eaa858e19fd55e7ac0452f7d90a5b6b1a298
tiagobica <tiago.bica@codurance.com>
2022-09-07 10:36:03 +0100

    Add string[] internal state

--- src/main/java/game/TicTacToe.java
+++ src/main/java/game/TicTacToe.java
@@ -2,13 +2,18 @@ package game;
 
 public class TicTacToe {
     public String play(int coordinate) {
-        if (coordinate == 3) {
-            return "..X\n...\n...";
-        }
-        if (coordinate == 5) {
-            return "...\n.X.\n...";
-        }
 
-        return "X..\n...\n...";
+        String[] board = new String[] {
+                ".", ".", ".",
+                ".", ".", ".",
+                ".", ".", "."
+        };
+
+        board[coordinate - 1] = "X";
+
+        return board[0] + board[1] + board[2] + "\n" +
+               board[3] + board[4] + board[5] + "\n" +
+               board[6] + board[7] + board[8]
+        ;
     }
 }
